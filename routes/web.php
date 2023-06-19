7ea70d1b7ae2e8988534ee8d6df7003941eb6bb0
David-1212 <david.vargas7480@alumnos.udg.mx>
2023-06-18 23:42:11 -0600

    se agrego la plantilla y se agrego el inicio y cierre de sesion de ususarios

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AcademicoController;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +13,20 @@ use App\Http\Controllers\AcademicoController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/index', function () {
+    return view('index');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('academico', AcademicoController::class);
+
+Route::resource('registrar', UserController::class);
+
+Route::post('/logout', function () {
+    Auth::logout(); // Cierra la sesión del usuario
+    return redirect('/index'); // Redirige a la página de inicio u otra página de tu elección
+});
+
+
