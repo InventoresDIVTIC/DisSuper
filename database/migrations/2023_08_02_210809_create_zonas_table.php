a44947a9055bb157a1502055c8181944a2806c80
MasterDigimon <gamerdigimonxl@gmail.com>
2023-08-02 15:16:53 -0600

    Corrección en Migraciones de Zonas, acciones y add_contraseñaSeguridad

--- database/migrations/2023_08_02_210809_create_zonas_table.php
+++ database/migrations/2023_08_02_210809_create_zonas_table.php
@@ -11,42 +11,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('acciones');
-        Schema::create('acciones', function (Blueprint $table) {
-            $table->id('id_accion')->unique()->required();
-            $table->unsignedBigInteger('user_id');
-            $table->string('nombre_accion');
-            $table->timestamp('fecha_accion')->required();
-            $table->string('SQL')->required();
-            $table->string('status')->required();
-            
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
-
-        Schema::dropIfExists('zonas');
 
         Schema::create('zonas', function (Blueprint $table) {
             $table->id('id_zona')->unique()->required();
             $table->string('nombre_zona');
             $table->unsignedBigInteger('user_id');
+            $table->timestamps();
         });
 
         DB::table('zonas')->insert([
-            'id_zona' => 0,
+            'id_zona' => 1,
             'nombre_zona' => 'Sin Zona',
             'user_id' => 1,
         ]);
-        
 
+        
         Schema::table('empleados', function (Blueprint $table) {
             $table->unsignedBigInteger('id_zona')->default(0)->nullable();
 
             $table->foreign('id_zona')->references('id_zona')->on('zonas')->onDelete('cascade');
         });
 
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('contraseña_seguridad')->default(bcrypt('123456789'));
-        });
     }
 
     /**
@@ -54,20 +39,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function(Blueprint $table){
-            $table->dropColumn('contraseña_seguridad');
-        });
-
+        
         Schema::table('empleados', function (Blueprint $table){
             $table->dropForeign('id_zona');
         });
-
-        Schema::dropIfExists('acciones');
-
         Schema::dropIfExists('zonas');
-
-        
-
-        
     }
 };
---
+++ database/migrations/2023_08_02_211013_create_acciones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('acciones', function (Blueprint $table) {
+            $table->id('id_accion')->unique()->required();
+            $table->unsignedBigInteger('user_id');
+            $table->string('nombre_accion');
+            $table->timestamp('fecha_accion')->required();
+            $table->string('SQL')->required();
+            $table->string('status')->required();
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('acciones');
+    }
+};
