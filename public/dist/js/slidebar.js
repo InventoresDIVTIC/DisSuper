f3c2e49b65498086bf49793662b2977d43c9f45a
MasterDigimon <gamerdigimonxl@gmail.com>
2023-11-01 21:20:38 -0600

    Solucion bug del slider y rediseño de vista de llamada de atencion de empleado

--- public/dist/js/slidebar.js
+++ public/dist/js/slidebar.js
@@ -23,58 +23,138 @@ $(document).ready(function() {
         });
     }
 
+    function primerIndicador(){
+        var nuevoIndicador = `
+        <div class="indicador">
+            <div class="card">
+                <div class="card-header">
+                <label class="col-sm-2 card-title text-info">Indicador ${indicadorCount}</label>
+                </div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Nombre del Indicador ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <select name="indicador${indicadorCount}" id="indicador${indicadorCount}" class="form-control">
+                                <option value="Productividad">Productividad</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Hallazgo ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <textarea rows="3" class="form-control" placeholder="Explique sus Hallazgos"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Evidencia ${indicadorCount}</label>
+                        <div class="col-sm-9">
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="customFile${indicadorCount}">
+                                    <label class="custom-file-label" for="customFile${indicadorCount}">Imagen de Evidencia</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Sistemas de Referencia ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Sistemas de Referenciar">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2 col-form-label">Valor Alcanzado ${indicadorCount}</label>
+                        <div class="col-md-10">
+                            <input type="range" class="form-range" min="0" max="100" style="width: 90%;">
+                            <output class="output-range-value">0</output>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Afectaciones ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <textarea rows="3" class="form-control" placeholder="Explique las Afectaciones al Indicador"> </textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Normas Incumplidas ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Normas Incumplidas">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+
+        // Convertir el nuevo indicador en un elemento jQuery
+        var $nuevoIndicador = $(nuevoIndicador);
+
+        // Agregar el nuevo indicador al contenedor
+        $("#indicadores-container").append($nuevoIndicador);
+
+        // Inicializar el slidebar para el nuevo indicador dinámico
+        initSlidebarForElement($nuevoIndicador);
+    }
+
+
     // Manejar el clic en el botón "Agregar Indicador"
     $("#addIndicador").click(function() {
         indicadorCount++;
         // Crear un nuevo conjunto de campos para el indicador dinámico
         var nuevoIndicador = `
         <div class="indicador">
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Nombre del Indicador ${indicadorCount}</label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Nombre del Indicador">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Hallazgo ${indicadorCount}</label>
-                <div class="col-md-9">
-                    <textarea rows="3" class="form-control" placeholder="Explique sus Hallazgos"></textarea>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Evidencia ${indicadorCount}</label>
-                <div class="col-sm-9">
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="customFile${indicadorCount}">
-                            <label class="custom-file-label" for="customFile${indicadorCount}">Imagen de Evidencia</label>
+            <div class="card">
+                <div class="card-body">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Indicador ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <select name="indicador${indicadorCount}" id="indicador${indicadorCount}" class="form-control">
+                            <option value="Productividad">Productividad</option>
+                            </select>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Sistemas de Referencia ${indicadorCount}</label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Sistemas de Referenciar">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label">Valor Alcanzado ${indicadorCount}</label>
-                <div class="col-md-10">
-                    <input type="range" class="form-range" min="0" max="100" style="width: 90%;">
-                    <output class="output-range-value">0</output>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Afectaciones ${indicadorCount}</label>
-                <div class="col-md-9">
-                    <textarea rows="3" class="form-control" placeholder="Explique las Afectaciones al Indicador"> </textarea>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Normas Incumplidas ${indicadorCount}</label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Normas Incumplidas">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Hallazgo ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <textarea rows="3" class="form-control" placeholder="Explique sus Hallazgos"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Evidencia ${indicadorCount}</label>
+                        <div class="col-sm-9">
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="customFile${indicadorCount}">
+                                    <label class="custom-file-label" for="customFile${indicadorCount}">Imagen de Evidencia</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Sistemas de Referencia ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Sistemas de Referenciar">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2 col-form-label">Valor Alcanzado ${indicadorCount}</label>
+                        <div class="col-md-10">
+                            <input type="range" class="form-range" min="0" max="100" style="width: 90%;">
+                            <output class="output-range-value">0</output>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Afectaciones ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <textarea rows="3" class="form-control" placeholder="Explique las Afectaciones al Indicador"> </textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"> Normas Incumplidas ${indicadorCount}</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" placeholder="Normas Incumplidas">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -89,4 +169,6 @@ $(document).ready(function() {
         // Inicializar el slidebar para el nuevo indicador dinámico
         initSlidebarForElement($nuevoIndicador);
     });
+
+    primerIndicador();
 });
--- resources/views/empleados/show.blade.php
+++ resources/views/empleados/show.blade.php
@@ -260,61 +260,7 @@
 
         <div class="Indicadores" id="indicadores-container">
             <!-- Campos del primer indicador -->
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Nombre del Indicador </label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Nombre del Indicador">
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Hallazgo </label>
-                <div class="col-md-9">
-                    <textarea rows="3" class="form-control" placeholder="Explique sus Hallazgos"></textarea>
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label">Evidencia</label>
-                <div class="col-sm-9">
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="customFile">
-                            <label class="custom-file-label" for="customFile">Imagen de Evidencia</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Continuación de campos del primer indicador -->
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Sistemas de Referencia</label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Sistemas de Referenciar">
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label class="col-md-2 col-form-label">Valor Alcanzado</label>
-                <div class="col-md-10">
-                    <input type="range" class="form-range" min="0" max="100" id="static-indicador-1" name="static-indicador-1"style="width: 90%;">
-                    <output id="static-indicador-1">0</output>
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Afectaciones</label>
-                <div class="col-md-9">
-                    <textarea rows="3" class="form-control" placeholder="Explique las Afectaciones al Indicador"> </textarea>
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label"> Normas Incumplidas</label>
-                <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Normas Incumplidas">
-                </div>
-            </div>
+            
         </div>
 
         <div class="form-group row">
@@ -343,6 +289,7 @@
             <button class="btn add-btn btn-info" id="addIndicador">Agregar Indicador</button>
         </div>
     </div>
+    
     
 </div><!-- /.tab-pane -->
 
