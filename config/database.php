121f57db9cd0bf6af4f091495da41b7006546fd6
MasterDigimon <gamerdigimonxl@gmail.com>
2024-02-08 23:14:22 -0600

    Update for Heroku database config

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 return [
 
@@ -48,19 +48,19 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            /*
+            
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            */
+            /*
             'host' => $url["host"],
             'port' => $url["port"],
             'database' => substr($url["path"], 1),
             'username' => $url["user"],
             'password' => $url["pass"],
-
+*/
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -71,6 +71,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            
         ],
 
         'pgsql' => [
