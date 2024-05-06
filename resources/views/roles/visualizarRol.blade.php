5da886d7c1a51af67467ef2f5641b484e6737c96
David Guadalupe Vargas Lopez <70907218+David-1212@users.noreply.github.com>
2024-05-05 21:26:19 -0600

    Update visualizarRol.blade.php

--- resources/views/roles/visualizarRol.blade.php
+++ resources/views/roles/visualizarRol.blade.php
@@ -5,19 +5,19 @@
     <div class="card-header">
         <div class="row">
             <div class="col-md-12 text-center text-info">
-                <h3>Roles<h3>
+                <h3>Roles</h3>
             </div>
         </div>
     </div>
     <div class="card-body">
         <div class="row">
             <div class="col-md-6 offset-md-3 text-center">
-                <!-- Botón de agregar centrado -->
                 <div class="text-center">
                     <form action="{{ route('roles.store') }}" method="POST" class="form-inline">
                         @csrf
                         <div class="input-group">
-                            <input class="form-control" id="NombreNuevoRol" name="NombreNuevoRol" placeholder="Nombre del Nuevo Rol" style="width: 500px;">
+                            <input class="form-control" id="NombreNuevoRol" name="NombreNuevoRol" placeholder="Nombre del Nuevo Rol" style="width: 300px;">
+                            <input type="number" id="Npermisos" name="Npermisos" min="1" max="5" class="form-control ml-2" style="width: 100px;">
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="submit">
                                     <i class="fas fa-plus"></i> Agregar Rol
@@ -26,41 +26,38 @@
                         </div>
                     </form>
                 </div>
-                
             </div>
         </div>
         <br>
-
-        
         <br>
         <table id="tablContratos" class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th class="text-center">Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($roles as $roles)
+            <thead>
                 <tr>
-                    <td class="text-center">{{ $roles->id }}</td>
-                    <td class="text-center">{{ $roles->name }}</td>
-                    <td class="text-center">
-                        <div class="btn-group">
-                            <form action="{{ route('roles.destroy', $roles->id) }}" method="POST" style="display: inline-block;">
-                                @method('DELETE')
-                                @csrf
-                                <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('¿Estás seguro de que deseas eliminar este contrato?')">
-                                    <i class="fas fa-trash"></i> Eliminar
-                                </button>
-                            </form>
-                        </div>
-                    </td>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th class="text-center">Opciones</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($roles as $rol)
+                    <tr>
+                        <td class="text-center">{{ $rol->id }}</td>
+                        <td class="text-center">{{ $rol->name }}</td>
+                        <td class="text-center">
+                            <div class="btn-group">
+                                <form action="{{ route('roles.destroy', $rol->id) }}" method="POST" style="display: inline-block;">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('¿Estás seguro de que deseas eliminar este contrato?')">
+                                        <i class="fas fa-trash"></i> Eliminar
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
         <br><br><br>
     </div>
 </div>
