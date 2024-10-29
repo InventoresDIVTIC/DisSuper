32c82829fa6cf2ac8eaf51d22b1e109a44d48c06
jose <122117795+mgtzjj@users.noreply.github.com>
2024-10-29 01:29:35 -0600

    Fix Typo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Proyecto DiSuper{{ env('TESTQ')}} .</title>
-
+  <title>Proyecto DisSuper</title>
+<!-- ToDo: check if it is in use -->
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
